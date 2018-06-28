f860d6f8f130390b358833dc3f29f4f21e4c87be
ERIC KELLY <eakelly220@gmail.com>
2018-06-28 22:01:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,9 @@
 def call(env)
    resp = Rack::Response.new
    req = Rack::Request.new(env)
-
+   @@items = []
    if req.path=="/items"
+     item = req.params["item"]
      resp.write "You requested the songs"
    else
      resp.write "Route not found"
