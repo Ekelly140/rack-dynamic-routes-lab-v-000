06fe507eab0b86c0fd4c74efae9be3294a5109d8
ERIC KELLY <eakelly220@gmail.com>
2018-06-28 22:38:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ def call(env)
    resp = Rack::Response.new
    req = Rack::Request.new(env)
    if req.path.match(/items/)
-     item = req.path.split("/items/")
+     item = req.path.split("/items/").last
      binding.pry
      if @@items.include?(item)
        item.price
